edf01eb4e04992ef642f9975e3c3458cd4d507ae
kv0630028 <kv0630028@gmail.com>
2026-09-05 11:21:51 +0900

    마우스 조준 및 공격 방향 구현

--- Assets/Script/Player/PlayerAttack.cs
+++ Assets/Script/Player/PlayerAttack.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
@@ -6,10 +7,12 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private Transform firePoint;
 
     private PlayerInputActions inputActions;
+    private Camera mainCamera;
 
     void Awake()
     {
         inputActions = new PlayerInputActions();
+        mainCamera = Camera.main;
     }
 
     void OnEnable()
@@ -24,10 +27,23 @@ public class PlayerAttack : MonoBehaviour
 
     void Update()
     {
+        AimFirePoint();
+
         if (inputActions.Player.Attack.WasPressedThisFrame())
         {
             Debug.Log("Attack");
             Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         }
+    }
+
+    void AimFirePoint()
+    {
+        Vector3 screenPosition = Mouse.current.position.ReadValue();
+        screenPosition.z = Mathf.Abs(mainCamera.transform.position.z);
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(screenPosition);
+
+        Vector2 direction = mouseWorldPosition - firePoint.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        firePoint.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
